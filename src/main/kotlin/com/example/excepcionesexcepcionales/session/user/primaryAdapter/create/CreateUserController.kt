9cdae8065f71a4165e47535e230efb09d22aa763
rgomez <rafael.gomez@code.seat>
2023-10-20 18:44:55 +0200

    ADD Result Scaffolding to Create User Controller

--- src/main/kotlin/com/example/excepcionesexcepcionales/session/user/primaryAdapter/create/CreateUserController.kt
+++ src/main/kotlin/com/example/excepcionesexcepcionales/session/user/primaryAdapter/create/CreateUserController.kt
@@ -2,7 +2,17 @@ package com.example.excepcionesexcepcionales.session.user.primaryAdapter.create
 
 import com.example.excepcionesexcepcionales.session.user.application.create.CreateUserCommand
 import com.example.excepcionesexcepcionales.session.user.application.create.CreateUserCommandHandler
+import com.example.excepcionesexcepcionales.session.user.application.create.CreateUserResult
+import com.example.excepcionesexcepcionales.session.user.application.create.CreateUserResult.InvalidEmail
+import com.example.excepcionesexcepcionales.session.user.application.create.CreateUserResult.InvalidName
+import com.example.excepcionesexcepcionales.session.user.application.create.CreateUserResult.InvalidPhoneNumber
+import com.example.excepcionesexcepcionales.session.user.application.create.CreateUserResult.InvalidSurname
+import com.example.excepcionesexcepcionales.session.user.application.create.CreateUserResult.Success
+import com.example.excepcionesexcepcionales.session.user.application.create.CreateUserResult.Unknown
+import com.example.excepcionesexcepcionales.session.user.application.create.CreateUserResult.UserAlreadyExists
 import com.example.excepcionesexcepcionales.shared.clock.Clock
+import com.example.excepcionesexcepcionales.shared.error.Response
+import com.example.excepcionesexcepcionales.shared.error.withoutBody
 import com.example.excepcionesexcepcionales.shared.id.IdGenerator
 import com.example.excepcionesexcepcionales.solution.user.domain.Email.InvalidEmailException
 import com.example.excepcionesexcepcionales.solution.user.domain.Name.InvalidNameException
@@ -72,3 +82,14 @@ class CreateUserController(
         fun handleNotFound(e: InvalidPhoneNumberException) = INVALID_PHONE_NUMBER
     }
 }
+
+//    private fun CreateUserResult.toServerError(): Response<*> =
+//        when(this) {
+//            InvalidEmail -> Response.badRequest().body(INVALID_EMAIL)
+//            InvalidName -> Response.badRequest().body(INVALID_NAME)
+//            InvalidPhoneNumber -> Response.badRequest().body(INVALID_PHONE_NUMBER)
+//            InvalidSurname -> Response.badRequest().body(INVALID_SURNAME)
+//            UserAlreadyExists -> Response.status(CONFLICT).body(INVALID_SURNAME)
+//            is Success -> Response.status(CREATED).withoutBody()
+//            is Unknown -> throw reason
+//        }
